0e62b778b91d0098a23e3c0166026800eb4d2a1d
Matheus Albuquerque <mvca@cin.ufpe.br>
2019-05-05 20:35:29 -0300

    finished project

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@
 using namespace std;
 int mainOperator(string expression, int* op);
 
-string checkParentheses(string expression){
-    int pOpen = 0;
-    int pClose = 0;
-    int indexUltOpen;
-    int indexUltClose;
+string checkParentheses(string expression){ //função verifica e corrige a quantidade de paratenses numa expressão
+    int pOpen = 0; //numero de paratenses abertos
+    int pClose = 0; //numero de parenteses fechado
+    int indexUltOpen; //indice do ultimo paresentese aberto da string
+    int indexUltClose; //indice do ultimo paresentese fechado da string
     
-    for(int i = 0; i < expression.size(); i++){
+    for(int i = 0; i < expression.size(); i++){ //varre a string contando o numero de parenteses
         if(expression[i] == '('){
             pOpen++;
             indexUltOpen = i;
@@ -28,16 +28,16 @@ string checkParentheses(string expression){
             indexUltClose = i;
         }
     }
-    while(pOpen > pClose){
+    while(pOpen > pClose){ //caso tenha mais parenteses abertos do que fechado
        expression.erase(expression.begin());
        pOpen--;    
     }
-    while(pOpen < pClose){
+    while(pOpen < pClose){ //caso tenha mais parenteses fechados do que aberto
         expression.pop_back();
         pClose--;
     }
 
-    return expression;
+    return expression; //retorna expressão corrigida
 }
 // Aplicar regras.
 vector<Node*> applyRule(Node* node) {
@@ -52,21 +52,22 @@ vector<Node*> applyRule(Node* node) {
      * pega o valor verdade;
      * aplica a regra
      */
-    mainOP = mainOperator(node->getExpression(), &sbOperator);
-
-    if(sbOperator != -1 && sbOperator != NOT){
+    mainOP = mainOperator(node->getExpression(), &sbOperator); //Retorna a posição do operador principal da expressão, e qual é o operador
+                                                                            
+    if(sbOperator != -1 && sbOperator != NOT){ //para operadores binários, separa-se em duas sub-expressões
         subExpL = node->getExpression().substr(0, mainOP - 1);
         subExpR = node->getExpression().substr(mainOP + 2);
         subExpL = checkParentheses(subExpL);
         subExpR = checkParentheses(subExpR);
     }
-    else if(sbOperator == NOT){
+    else if(sbOperator == NOT){ //pega a expressão negada
         subExp = node->getExpression().substr(mainOP + 1);
         subExp = checkParentheses(subExp);
     }
 
 
-    //atribuindo valores
+    /*atribuindo valores
+    verificando cada operador e o valor verdade associado*/
     if(sbOperator == AND && node->getTruthValue() == 1){
         insertedNodes = node->insertFront(subExpL, 1, subExpR, 1);
     }
@@ -97,18 +98,23 @@ vector<Node*> applyRule(Node* node) {
     return insertedNodes;
 }
 
+//verifica se há contradição entre os nós
 void checkContradictions(vector<Node*> insertedNodes) {
+    
     for (int i = 0; i < insertedNodes.size(); i++) {
         if(insertedNodes[i]->checkContradiction())
             insertedNodes[i]->markContradiction();
     }
 }
 
+//encontra o operador principal da expressão e sua posição
 int mainOperator(string expression, int* op){
     int pOpen = 0;
     int pClose = 0;
     int position;
 
+    /*Usando o fato em que ao encontrar um operador e o numero de parenteses fechado == numero de parenteses aberto -1,
+    então esse operador é o operador principal*/
     for(int i=0; i < expression.size();i++){
         if(expression[i] == '(')
             pOpen++;
@@ -127,40 +133,41 @@ int mainOperator(string expression, int* op){
             break;
         }
     }
-    if(pOpen == 0 && pClose == 0){
+    if(pOpen == 0 && pClose == 0){//caso o elemento seja atomico
         position = -1;
         *op = -1;
     }
     return position;
 }
 
+//separa as proposições individualmente de um conjunto de proposições
 vector<string> separateSet(string set){
     vector<string> expressions;
 
     size_t position;
     int i = 0;
 
+    //separa e salva as proposições
     do{
         position = set.find(",");
-       // cout<< "position: "<<position<<endl;
 
         if(position != string::npos){
 
             expressions.push_back(set.substr(0, position));
             set = set.substr(position + 2);
-           // cout << expressions[i] <<" "<< "A"<<endl;
             i++;
         }
         else{
             expressions.push_back(set.substr(0));
-            //cout << expressions[i] <<" "<< "A"<<endl;;
         }
     }while(position != string::npos);
 
     return expressions;
 }
 
-bool verifyAlpha(vector<Node*> nodes, int* index){
+/*verifica qual proposição, junto com o seu valor verdade, é do tipo alfa.
+  retornando o indice do vetor no qual esse nó é do tipo alfa, e um booleano dizendo se existe uma proposição do tipo alfa*/
+bool verifyAlpha(vector<Node*> nodes, int* index){ 
     bool isAlpha = false;
     string expression;
     int op;
@@ -206,7 +213,8 @@ int main() {
     ifstream inFile;
     ofstream outFile;
     
-    outFile.open("Saida.out", ios::out);
+    //abertura arquivo
+    outFile.open("Saida.out", ios::out); 
     inFile.open("Entrada.in", ios::in);
     if (! outFile) {  
         cout << "Arquivo Saida.out nao pode ser aberto" << endl;    
@@ -216,73 +224,69 @@ int main() {
         cout << "Arquivo Entrada.in nao pode ser aberto" << endl;   
         abort();
     }
-
+    //leitura número de casos
     getline(inFile, N);
     n = stoi(N);
 
     for(int problem = 0; problem < n; problem++){
-        getline(inFile,text);
+        getline(inFile,text); //leitura do problema
         text.pop_back();
 
+        //Separa do que foi lido, as proposições e o problema
         size_t found_index = text.find(found_letter);
-       // cout<<"fi="<< found_index<<endl;
-        expression = text.substr(0,found_index - 1);
+        expression = text.substr(0,found_index - 1); //separa a expressão
         
-        if(text[found_index + 2] == 'c'){
-            size_t found_index = text.find("{");
-
+        if(text[found_index + 2] == 'c'){ //caso seja do formato "e consequencia logica"
+            size_t found_index = text.find("{"); //pega o conjunto de sentenças
             set = text.substr(found_index + 1);
             set.pop_back();
             set.pop_back();
-            question = "consequencia logica?";
-            //cout<< "expression:" << expression <<spc <<"expression2:"<< set <<spc<< "question:"<< question << endl;
+            question = "consequencia logica?"; //atribui a pergunta
         }
-        else{ 
-            question = text.substr(found_index + 2);//, text.size() - (found_index + 2) );
-           // cout << question.size() <<endl;
-         //   cout<< "expression:" << expression <<spc << "question:"<< question<< endl;    
+        else{ //caso nao seja do formato de consequencia logica
+            question = text.substr(found_index + 2); //pega a pergunta
         }
 
+        //verifica a pergunta
         if(question == "satisfativel?"){
-            //cout<< "entrou Satisf"<<endl;
+            /*para verificar se uma proposição é satisfatível
+            perguntamos ao tableau se existe um mundo possível no qual ela é verdadeira
+            se existir, então ela é satisfatível*/
             Node tableau(expression, 1);
-
             vector<Node*> appliableNodes = tableau.getAppliableNodes();
             vector<Node*> insertedNodes;
             int index;
             bool isAlpha = false;
             while(!tableau.isClosed() && !appliableNodes.empty()){
-
+                
+                //dá-se uma preferência para a aplicação de regras do tipo alfa
                 for(int i = 0; i < appliableNodes.size(); i++){
                     isAlpha = false;
                     for(int j = 0; j < appliableNodes.size(); j++){
                         isAlpha = verifyAlpha(appliableNodes, &index);
                     } 
-                    if(!isAlpha)
+                    if(!isAlpha) //caso nao seja alfa, aplica-se a regra no nó disponível
                         insertedNodes = applyRule(appliableNodes[i]);
-                    else
+                    else //caso seja alfa, aplica-se a regra nele
                         insertedNodes = applyRule(appliableNodes[index]);
 
                     checkContradictions(insertedNodes);
                     appliableNodes = tableau.getAppliableNodes();
                 }
-                //appliableNodes = tableau.getAppliableNodes();
             }
             
             outFile << "Problema #"<<problem + 1<<endl;
             if(tableau.isClosed())
                 outFile << "Nao, nao e satisfativel."<< endl;
-                //std::cout << "Tableau fechado." << std::endl;
             else
                 outFile << "Sim, e satisfativel."<<endl;
             if(problem + 1 != n)
                 outFile << endl;
-            //tableau.printTree();
-        
-        
         }
         else if(question == "insatisfativel?"){
-            //cout<< "entrou INSatisf"<<endl;
+            /*para verificar se uma proposição é insatisfatível
+            perguntamos ao tableau se existe um mundo possível no qual ela é verdadeira,
+            se não existir, então ela é insatisfativel*/
             Node tableau(expression, 1);
 
             vector<Node*> appliableNodes = tableau.getAppliableNodes();
@@ -290,7 +294,7 @@ int main() {
             int index;
             bool isAlpha = false;
             while(!tableau.isClosed() && !appliableNodes.empty()){
-
+                //dá-se uma preferência para a aplicação de regras do tipo alfa
                 for(int i = 0; i < appliableNodes.size(); i++){
                     isAlpha = false;
                     for(int j = 0; j < appliableNodes.size(); j++){
@@ -313,10 +317,11 @@ int main() {
                 outFile<< "Nao, nao e insatisfativel."<< endl;  
             if(problem + 1 != n)
                 outFile << endl;
-            //tableau.printTree();
         }
         else if(question == "tautologia?"){
-            //cout<< "entrou TAUTOLOGIA"<<endl;
+            /*para verificar se uma proposição é tautologia
+            perguntamos ao tableau se existe um mundo possível no qual ela é falsa,
+            se não existir, então ela é tautologia*/
             Node tableau(expression, 0);
 
             vector<Node*> appliableNodes = tableau.getAppliableNodes();
@@ -324,7 +329,7 @@ int main() {
             int index;
             bool isAlpha = false;
             while(!tableau.isClosed() && !appliableNodes.empty()){
-
+                //dá-se uma preferência para a aplicação de regras do tipo alfa
                 for(int i = 0; i < appliableNodes.size(); i++){
                     isAlpha = false;
                     for(int j = 0; j < appliableNodes.size(); j++){
@@ -338,7 +343,6 @@ int main() {
                     checkContradictions(insertedNodes);
                     appliableNodes = tableau.getAppliableNodes();
                 }
-                //appliableNodes = tableau.getAppliableNodes();
             }
             outFile << "Problema #"<<problem + 1<<endl;
             if(tableau.isClosed())
@@ -347,12 +351,11 @@ int main() {
                 outFile << "Nao, nao e tautologia."<<endl;
             if(problem + 1 != n)
                 outFile << endl;
-                //std::cout << "Tableau fechado." << std::endl;
-
-            //tableau.printTree();
         }
         else if(question == "refutavel?"){
-            //cout<< "entrou REFUTAVEL"<<endl;
+            /*para verificar se uma proposição é refutável
+            perguntamos ao tableau se existe um mundo possível no qual ela é falsa,
+            se existir, então ela é refutável*/
             Node tableau(expression, 0);
 
             vector<Node*> appliableNodes = tableau.getAppliableNodes();
@@ -360,7 +363,7 @@ int main() {
             int index;
             bool isAlpha = false;
             while(!tableau.isClosed() && !appliableNodes.empty()){
-
+                //dá-se uma preferência para a aplicação de regras do tipo alfa
                 for(int i = 0; i < appliableNodes.size(); i++){
                     isAlpha = false;
                     for(int j = 0; j < appliableNodes.size(); j++){
@@ -374,7 +377,6 @@ int main() {
                     checkContradictions(insertedNodes);
                     appliableNodes = tableau.getAppliableNodes();
                 }
-                //appliableNodes = tableau.getAppliableNodes();
             }
             outFile << "Problema #"<<problem + 1<<endl;
             if(tableau.isClosed())
@@ -384,25 +386,25 @@ int main() {
             if(problem + 1 != n)
                 outFile << endl;
 
-            //tableau.printTree();
         }
         else if(question == "consequencia logica?"){
-            //cout<< "entrou CL"<<endl;
+            /*para verificar se uma proposição é consequencia logica de um conjunto de proposições
+            perguntamos ao tableau se existe um mundo possível no qual ela a proposição é falsa, e
+            as proposições do conjunto são verdadeiras se não existir, então ela é consequencia logica do conjunto*/
             Node tableau(expression, 0);
             vector<string> expressions;
-            expressions = separateSet(set);
-            ;
-            
-            for(int i = 0; i < expressions.size(); i++){
-                tableau.insertFront(expressions[i], 1);   
-            }
-
-            vector<Node*> appliableNodes = tableau.getAppliableNodes();
+            expressions = separateSet(set);//separa as proposições do conjunto
             vector<Node*> insertedNodes;
+
+            for(int i = 0; i < expressions.size(); i++){
+                insertedNodes = tableau.insertFront(expressions[i], 1); //coloca elas no tableau, depois verifica se existe contradição
+                checkContradictions(insertedNodes);
+            }
+            vector<Node*> appliableNodes = tableau.getAppliableNodes();
             int index;
             bool isAlpha = false;
             while(!tableau.isClosed() && !appliableNodes.empty()){
-
+                //dá-se uma preferência para a aplicação de regras do tipo alfa
                 for(int i = 0; i < appliableNodes.size(); i++){
                     isAlpha = false;
                     for(int j = 0; j < appliableNodes.size(); j++){
@@ -416,7 +418,6 @@ int main() {
                     checkContradictions(insertedNodes);
                     appliableNodes = tableau.getAppliableNodes();
                 }
-                //appliableNodes = tableau.getAppliableNodes();
             }
 
             outFile << "Problema #"<<problem + 1<<endl;
@@ -427,6 +428,8 @@ int main() {
             
             if(problem + 1 != n)
                 outFile << endl;
+
+            //tableau.printTree();
         }
 
     }  
